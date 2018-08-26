4451638b3024ce31fc7fa1e0a17fcf65be419eca
rxhlv <->
2018-08-26 15:02:11 -0500

    Initial commit

---
+++ src/main/java/com/rxhlv/base/baseconfigserver/BaseConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.rxhlv.base.baseconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseConfigServerApplication.class, args);
+	}
+}
